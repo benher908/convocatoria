fbbfae6c44e6d6ef48739b7a6ad72d9982d72b76
benher908 <benkisher908@gmail.com>
2025-08-18 12:31:02 -0600

    cambios

--- preseausuarios/app/(public)/layout.js
+++ preseausuarios/app/(public)/layout.js
@@ -1,13 +1,14 @@
 // frontend/app/(public)/layout.js
-import Navbar from '../../components/Navbar'
-import Footer from '../../components/Footer'
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+import '../../styles/globals.css';
 
 export default function PublicLayout({ children }) {
-    return (
+  return (
     <>
-        <Navbar />
-            <main>{children}</main>
-        <Footer />
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
     </>
-)
+  );
 }
--- preseausuarios/app/layout.js
+++ preseausuarios/app/layout.js
@@ -1,26 +1,28 @@
-// frontend/app/layout.js
-import '../styles/globals.css'
+import { Inter } from 'next/font/google';
+import '../styles/globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Presea STEM',
   description: 'Sistema de registro - Proyecto STEM',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
+    <html lang="es" className={inter.className}>
       <head>
-        <link 
-          rel="stylesheet" 
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" 
-          integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" 
-          crossOrigin="anonymous" 
-          referrerPolicy="no-referrer" 
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+          xintegrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
         />
       </head>
       <body>
         {children}
       </body>
     </html>
-  )
+  );
 }
